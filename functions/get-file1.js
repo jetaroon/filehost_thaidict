a88b58e9cfc0d9968f0e397a583855440590eba2
jetaroon <jetaroon@gmail.com>
2025-05-27 22:50:32 +0700

    first commit

---
+++ functions/get-file1.js
@@ -0,0 +1,19 @@
+// Netlify serverless function (Node.js)
+exports.handler = async (event, context) => {
+  const fs = require('fs');
+  const path = require('path');
+
+  const filePath = path.resolve(__dirname, '../public/normal_dict.bin');
+  const data = fs.readFileSync(filePath);
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/octet-stream',
+      'Access-Control-Allow-Origin': '*',
+      'Content-Disposition': 'inline; filename="normal_dict.bin"',
+    },
+    body: buffer.toString('base64'), // Send as base64
+    isBase64Encoded: true,
+  };
+};
